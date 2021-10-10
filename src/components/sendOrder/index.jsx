661177d8fea8670ccc0ead3a2f212030649b970a
yangxuan <1130180692@qq.com>
2021-10-10 20:27:18 +0800

    支付接口

--- src/components/sendOrder/index.jsx
+++ src/components/sendOrder/index.jsx
@@ -9,9 +9,38 @@ export default function Index() {
     //   timeStamp: new Date().getTime(),
 
     // })
+    let data = {
+      openid: Taro.getStorageSync('userInfo').openid,
+      money: 0.1
+    }
+    Taro.request({
+      url: "http://82.157.124.108:8087/wxpay",
+      // url: "http://82.157.124.108:8087/wxpay",
+      data: data,
+      method: "POST",
+      dataType: "json",
+      success: (res) => {
+        let data = res.data.data
+        Taro.requestPayment({
+          timeStamp: data.timeStamp,
+          package: "prepay_id=" + data.prepayId,
+          nonceStr: data.nonceStr,
+          signType: "MD5",
+          paySign: data.paySign,
+          success: (res) => {
+            console.log("支付成功", res)
+          },
+          fail: (res) => {
+            console.log("支付失败", res)
+          },
+          // complete: function (res) { }
+        })
+      }
+    })
+
   }
   return (
-    <View style={{marginTop:'100px'}}>
+    <View style={{ marginTop: '100px' }}>
       <Button onClick={payFunc}>发起支付</Button>
     </View>
   )
